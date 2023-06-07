73e2c69b6f0fa2d1c79eb31bbe2c0815b5740a70
GABRIEL DE SOUZA FURTADO <121953504+gabrielsousaf@users.noreply.github.com>
2023-06-06 21:27:38 -0300

    Config Timer

--- src/App.js
+++ src/App.js
@@ -2,34 +2,69 @@ import React, { useEffect, useState } from 'react'
 import GameBoard from './components/GameBoard';
 import GameOver from './components/GameOver'
 import game from './game/game'
+
+
 export default function MemoryGame() {
 
     const [gameOver, setGameOver] = useState(false);
     const [cards, setCards] = useState([])
+    const [timeLeft, setTimeLeft] = useState(60);
 
     useEffect(() => {
         setCards(game.createCardsFromTechs())
     }, [])
 
-    function restart() {
-        game.clearCards()
-        setCards(game.createCardsFromTechs())
-        setGameOver(false)
+    useEffect(() => {
+        const timer = setInterval(() => {
+            setTimeLeft(prevTime => prevTime -1);
+        }, 1000);
+        return() => {
+            clearInterval(timer)
+        }
+    })
+
+    useEffect(() => {
+        if (timeLeft === 0) {
+          setGameOver(true);
+        }
+      }, [timeLeft]
+    );
+      
+
+    function handleGameWin() {
+        setTimeLeft(60); 
     }
+      
+    function restart() {
+        game.clearCards();
+        setCards(game.createCardsFromTechs());
+        setGameOver(false);
+        handleGameWin(); // Reinicia o tempo
+    }
+      
 
     function handleFlip(card) {
-        game.flipCard(card.id, () => {
-            // GameOverCallback
+        if (gameOver) {
+            return;
+        }
+        else{
+            game.flipCard(card.id, () => {
+                // GameOverCallback
+                setGameOver(true)
+            }, () => {
+                //NoMatchCallback
+                setCards([...game.cards])
+            })
+            setCards([...game.cards]) 
+        }
+        if (timeLeft === 0){
             setGameOver(true)
-        }, () => {
-            //NoMatchCallback
-            setCards([...game.cards])
-        })
-        setCards([...game.cards])
+        }
     }
 
     return (
         <div>
+            <div id="timer"> Voçê têm {Math.floor(timeLeft / 60)}:{(timeLeft % 60).toString().padStart(2, '0')}s para completar o jogo</div>
             <GameBoard handleFlip={handleFlip} cards={cards}></GameBoard>
             <GameOver show={gameOver} handleRestart={restart}></GameOver>
         </div>
--- src/components/GameOver.js
+++ src/components/GameOver.js
@@ -1,12 +1,14 @@
 import React, { Fragment } from 'react'
 
 export default function GameOver(props) {
-    return (props.show ?
+    return (
+        props.show ?
         <div id="gameOver">
-            <div>
-                Parabéns, você completou o jogo!
-        </div>
-            <button id="restart" onClick={props.handleRestart} >Jogue novamente</button>
+          <div>
+           Deseja inicar outra partida?
+          </div>
+          <button id="restart" onClick={props.handleRestart}>Jogue novamente</button>
         </div> : <Fragment />
-    )
+      )
+      
 }
